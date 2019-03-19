2aa4f9ff060aff527dc160c9298b2bcc8a65ea8f
YouBeen Kim <qlqjsk@gmail.com>
2019-03-19 22:53:17 +0900

    Set testting directories

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+
+gulp.task('default', cb => {
+    //노드 소스
+    gulp.src('es6/**/*.js')
+        .pipe(babel())
+        .pipe(gulp.dest('dist'))
+        .on('end', function() {
+            console.log('done node es6');
+        });
+    //브라우저 소스
+    gulp.src('public/es6/**/*.js')
+        .pipe(babel())
+        .pipe(gulp.dest('public/dist'))
+        .on('end', function() {
+            console.log('done public es6');
+        });
+
+    cb();
+});
